9e5c71137ff551b2840543303b69d2756a74dbda
Vitor da Silva Nascimento <vitordasilvati@gmail.com>
2020-11-19 01:16:13 -0300

    project completed

---
+++ src/main/java/controller/ScreenViewController.java
@@ -0,0 +1,50 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScreenViewController implements Initializable {
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private ToggleButton btnView;
+
+    @FXML
+    private JFXTextField passwordView;
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+        btnView.setGraphic(new ImageView("/image/eyeOpen.png"));
+        passwordView.setVisible(false);
+        viewPass();
+
+    }
+
+    public void viewPass () {
+        btnView.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if(btnView.isSelected()){
+                    passwordView.setText(password.getText());
+                    password.setVisible(false);
+                    passwordView.setVisible(true);
+                    btnView.setGraphic(new ImageView("/image/eyeClosed.png"));
+
+                }else{
+                    password.setText(passwordView.getText());
+                    password.setVisible(true);
+                    passwordView.setVisible(false);
+                    btnView.setGraphic(new ImageView("/image/eyeOpen.png"));
+                }
+            }
+        });
+    }
+}
